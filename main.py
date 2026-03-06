d1c9509a4e965a3a69255debd4c3062e8769feb7
colinknox <colinwknox@gmail.com>
2026-03-05 20:16:11 -0500

    Created helper functions to output sanitized username

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+def strip_spaces(username):
+    new_username = ""
+    
+    for current_char in username:
+        if current_char == " ":
+            pass
+        else:
+            new_username += current_char
+
+    return new_username
+
+
+def lowercase(username):
+    new_username = ""
+    
+    for current_char in username.lower():
+        new_username += current_char
+
+    return new_username
+
+
+def remove_special_chars(username):
+    new_username = ""
+    
+    for current_character in username:
+        if current_character.isalnum():
+            new_username += current_character
+
+    return new_username
+
+
+
+def sanitize_username(username):
+    return lowercase(remove_special_chars(strip_spaces(username)))
+
+
+
 def main():
-    print("Hello from username-formatter!")
+    print(sanitize_username("123"))
 
 
 if __name__ == "__main__":
